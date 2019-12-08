372ae82c0b15e6f3f407719598cbdb15408bd515
Hanna <hanna.chaika@gmail.com>
2019-12-08 18:29:44 +0100

    Start with JUnit framework

---
+++ src/test/java/smoketests/PageTitleJUnit.java
@@ -0,0 +1,24 @@
+package smoketests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+public class PageTitleJUnit {
+
+    @Test
+    public void pageTitleTest(){
+        String webURL = "http://sdettraining.com/trguitransactions/AccountManagement.aspx";
+
+        WebDriver driver = utilities.DriverFactory.open("chrome");
+        driver.get(webURL);
+
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "SDET Training | Account Management";
+
+        Assert.assertEquals(actualTitle, expectedTitle);
+        // Assert.fail("Fail by purpose");
+
+
+    }
+}
