f02d43b9d1fb25175d3501350bb26b35371a6847
Hanna <hanna.chaika@gmail.com>
2019-12-08 21:35:32 +0100

    Setting Up a TestNG Framework

---
+++ src/test/java/smoketests/LoginPresentTestNG.java
@@ -0,0 +1,21 @@
+package smoketests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class LoginPresentTestNG {
+
+
+
+    @Test
+    public void loginElementsPresentTest(){
+        WebDriver driver = utilities.DriverFactory.open("chrome");
+        driver.get("http://sdettraining.com/trguitransactions/AccountManagement.aspx");
+
+        boolean loginEmailBox = driver.findElement(By.id("MainContent_txtUserName")).isSelected();
+        boolean passwordBox = driver.findElement(By.id("MainContent_txtPassword")).isSelected();
+
+
+    }
+}
