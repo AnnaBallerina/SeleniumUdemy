ac6c51f6d4ff467bd676f4a5caf171809a7298ce
Hanna <hanna.chaika@gmail.com>
2019-12-08 21:51:44 +0100

    TestNG assertions

--- src/test/java/smoketests/LoginPresentTestNG.java
+++ src/test/java/smoketests/LoginPresentTestNG.java
@@ -2,20 +2,21 @@ package smoketests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginPresentTestNG {
-
-
 
     @Test
     public void loginElementsPresentTest(){
         WebDriver driver = utilities.DriverFactory.open("chrome");
         driver.get("http://sdettraining.com/trguitransactions/AccountManagement.aspx");
 
-        boolean loginEmailBox = driver.findElement(By.id("MainContent_txtUserName")).isSelected();
-        boolean passwordBox = driver.findElement(By.id("MainContent_txtPassword")).isSelected();
+        boolean loginEmailBox = driver.findElement(By.id("MainContent_txtUserName")).isDisplayed();
+        boolean passwordBox = driver.findElement(By.id("MainContent_txtPassword")).isDisplayed();
 
+        Assert.assertTrue(loginEmailBox, "Email Testbox not present");
+        Assert.assertTrue(passwordBox, "Password Box not present");
 
     }
 }
