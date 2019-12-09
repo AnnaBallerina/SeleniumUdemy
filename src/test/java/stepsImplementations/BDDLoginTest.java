0a7472d8a73aff3f57380e921a062bc35e04ea81
Hanna <hanna.chaika@gmail.com>
2019-12-09 20:11:49 +0100

    Parameterizing Data into Step Definitions

--- src/test/java/stepsImplementations/BDDLoginTest.java
+++ src/test/java/stepsImplementations/BDDLoginTest.java
@@ -1,5 +1,6 @@
 package stepsImplementations;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -23,6 +24,17 @@ public class BDDLoginTest {
         driver.findElement(By.name("ctl00$MainContent$txtUserName")).sendKeys("tim@testemail.com");
         driver.findElement(By.name("ctl00$MainContent$txtPassword")).sendKeys("trpass");
         driver.findElement(By.name("ctl00$MainContent$btnLogin")).click();
+    }
+
+    @When("user enters correct (.*)")
+    public void user_enters_correct_username(String username){
+        System.out.println("TESTING " + username);
+        driver.findElement(By.name("ctl00$MainContent$txtUserName")).sendKeys(username);
+    }
+
+    @And("user enters correct <password>")
+    public void user_enters_correct_password(String password){
+        driver.findElement(By.name("ctl00$MainContent$txtPassword")).sendKeys(password);
     }
 
     @Then("^user gets confirmation$")
